e6dfa829f3f91e1320bd28afb78efc76635ee352
sunkeunchoi <639005+sunkeunchoi@users.noreply.github.com>
2025-10-15 21:40:04 +0900

    Completed 01 Setup 30:00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { ModeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <ModeToggle />
       <p className="font-bold text-rose-500">Hello World!</p>
+      <Button>Click Me</Button>
     </div>
   );
 }
